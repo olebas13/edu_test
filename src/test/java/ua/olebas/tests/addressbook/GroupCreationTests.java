c9517696102ed0f53ac19a937cf89b7838d45ccb
olebas1927 <olebas13@ukr.net>
2018-07-09 12:29:01 +0300

    fix

--- src/test/java/ua/olebas/tests/addressbook/GroupCreationTests.java
+++ src/test/java/ua/olebas/tests/addressbook/GroupCreationTests.java
@@ -2,31 +2,26 @@ package ua.olebas.tests.addressbook;
 
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class GroupCreationTests {
 	private WebDriver driver;
-	private String baseUrl;
-	private boolean acceptNextAlert = true;
-	private StringBuffer verificationErrors = new StringBuffer();
 
 	@BeforeClass(alwaysRun = true)
 	public void setUp() throws Exception {
 		driver = new FirefoxDriver();
-		baseUrl = "https://www.katalon.com/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
-
-	@Test
-	public void testUntitledTestCase() throws Exception {
 		driver.get("http://localhost/addressbook/");
 		driver.findElement(By.name("user")).clear();
 		driver.findElement(By.name("user")).sendKeys("admin");
 		driver.findElement(By.name("pass")).clear();
 		driver.findElement(By.name("pass")).sendKeys("secret");
 		driver.findElement(By.xpath("//input[@value='LOGIN']")).click();
+	}
+
+	@Test
+	public void testGroupCreation() throws Exception {
 		driver.findElement(By.linkText("GROUPS")).click();
 		driver.findElement(By.name("new")).click();
 		driver.findElement(By.name("group_name")).click();
@@ -45,19 +40,6 @@ public class GroupCreationTests {
 	@AfterClass(alwaysRun = true)
 	public void tearDown() throws Exception {
 		driver.quit();
-		String verificationErrorString = verificationErrors.toString();
-		if (!"".equals(verificationErrorString)) {
-			fail(verificationErrorString);
-		}
-	}
-
-	private boolean isElementPresent(By by) {
-		try {
-			driver.findElement(by);
-			return true;
-		} catch (NoSuchElementException e) {
-			return false;
-		}
 	}
 
 	private boolean isAlertPresent() {
@@ -66,21 +48,6 @@ public class GroupCreationTests {
 			return true;
 		} catch (NoAlertPresentException e) {
 			return false;
-		}
-	}
-
-	private String closeAlertAndGetItsText() {
-		try {
-			Alert alert = driver.switchTo().alert();
-			String alertText = alert.getText();
-			if (acceptNextAlert) {
-				alert.accept();
-			} else {
-				alert.dismiss();
-			}
-			return alertText;
-		} finally {
-			acceptNextAlert = true;
 		}
 	}
 }
