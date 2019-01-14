f519ee485491e34f76d39a4f50eaa5cdabe31697
olebas13 <o.nevoyt13@gmail.com>
2019-01-14 12:56:46 +0200

    fix GroupCreationTests

--- src/test/java/ua/olebas/tests/addressbook/model/GroupData.java
+++ src/test/java/ua/olebas/tests/addressbook/model/GroupData.java
@@ -3,12 +3,12 @@ package ua.olebas.tests.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-	private final String id;
+	private int id;
 	private final String name;
 	private final String header;
 	private final String footer;
 
-	public GroupData(String id, String name, String header, String footer) {
+	public GroupData(int id, String name, String header, String footer) {
 		this.id = id;
 		this.name = name;
 		this.header = header;
@@ -16,14 +16,28 @@ public class GroupData {
 	}
 
 	public GroupData(String name, String header, String footer) {
-		this.id = null;
+		this.id = 0;
 		this.name = name;
 		this.header = header;
 		this.footer = footer;
 	}
 
-	public String getId() {
+	public int getId() {
 		return id;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		GroupData groupData = (GroupData) o;
+		return id == groupData.id &&
+				Objects.equals(name, groupData.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
 	}
 
 	public String getName() {
@@ -46,17 +60,7 @@ public class GroupData {
 				'}';
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		GroupData groupData = (GroupData) o;
-		return Objects.equals(id, groupData.id) &&
-				Objects.equals(name, groupData.name);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, name);
+	public void setId(int id) {
+		this.id = id;
 	}
 }
--- src/test/java/ua/olebas/tests/addressbook/tests/groups/GroupCreationTests.java
+++ src/test/java/ua/olebas/tests/addressbook/tests/groups/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.*;
 import ua.olebas.tests.addressbook.model.GroupData;
 import ua.olebas.tests.addressbook.tests.TestBase;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -13,10 +14,22 @@ public class GroupCreationTests extends TestBase {
 	public void testGroupCreation() {
 		app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-		app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        GroupData group = new GroupData("test1", null, null);
+		app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
 
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        before.add(group);
+        int max = 0;
+        for (GroupData g : after) {
+        	if (g.getId() > max) {
+        		max = g.getId();
+			}
+		}
+        group.setId(max);
+
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 	}
 
 }
